4f22261e8bc789cf972f721cb76d1fec66c45ade
Rauf Rasulzada <rrasulzada15558@ada.edu.az>
2024-02-09 11:14:15 +0400

    create gradle project

---
+++ gradle-tutorial/app/src/main/java/az/edu/ada/wm2/App.java
@@ -0,0 +1,12 @@
+
+package az.edu.ada.wm2;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}

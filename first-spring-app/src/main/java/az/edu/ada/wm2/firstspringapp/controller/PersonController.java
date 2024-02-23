9dd04dda0eb66a6b6fdf2c90aa74a52027a83f53
Rauf Rasulzada <rrasulzada15558@ada.edu.az>
2024-02-23 11:19:43 +0400

    Class progress

--- first-spring-app/src/main/java/az/edu/ada/wm2/firstspringapp/controller/PersonController.java
+++ first-spring-app/src/main/java/az/edu/ada/wm2/firstspringapp/controller/PersonController.java
@@ -1,31 +1,42 @@
 package az.edu.ada.wm2.firstspringapp.controller;
 
-import java.util.List;
-
+import az.edu.ada.wm2.firstspringapp.service.PersonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import az.edu.ada.wm2.firstspringapp.model.Person;
 
 @Controller
 @RequestMapping("/persons")
+@RequiredArgsConstructor
 public class PersonController {
 
-  @Autowired
-  private Person defaultPerson;
+  private final PersonService personService;
 
-  @GetMapping()
+  @GetMapping({"", "/"})
   public String listPersons(Model model) {
-    System.out.println(defaultPerson);
-    model.addAttribute("persons", List.of(
-      new Person("Ali", "Aliyev"),
-      new Person("Gulnara", "Azizova"),
-      new Person("Murad", "Ibrahimli"),
-      new Person("Laman", "Khudatzada")
-    ));
+    model.addAttribute("persons", personService.listPersons());
     return "person_list";
+  }
+
+  @GetMapping("/new")
+  public String newForm(Model model){
+    model.addAttribute("person", new Person("", ""));
+    return "new_person_form";
+  }
+
+  @PostMapping("/save")
+  public String savePerson(Model model, @ModelAttribute("person") Person newPerson) {
+    personService.addPerson(newPerson);
+    return "redirect:/persons";
+  }
+
+  @GetMapping("/delete/{idx}")
+  public String deletePerson(@PathVariable Integer idx){
+    personService.deletePerson(idx);
+    return "redirect:/persons";
   }
 }
--- first-spring-app/src/main/java/az/edu/ada/wm2/firstspringapp/model/Person.java
+++ first-spring-app/src/main/java/az/edu/ada/wm2/firstspringapp/model/Person.java
@@ -1,35 +1,13 @@
 package az.edu.ada.wm2.firstspringapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person{
 	private String fName;
 	private String lName;
-
-	public Person(String fName, String lName) {
-		 this.fName = fName;
-		 this.lName = lName;
-	}
-
-	public String getfName() {
-		return fName;
-	}
-
-	public void setfName(String fName) {
-		this.fName = fName;
-	}
-	
-	public String getlName() {
-		 return lName;
-    }
-
-	public void setlName(String lName) {
-		this.lName = lName;
-	}
-
-	@Override
-	public String toString() {
-		return "Person{" +
-				"fName='" + fName + '\'' +
-				", lName='" + lName + '\'' +
-				'}';
-	}
 }
